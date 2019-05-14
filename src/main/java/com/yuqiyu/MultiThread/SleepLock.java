4ad33d53cd5b10d19d76dd3dd697339f742e1c50
andesmountain <443889956@qq.com>
2019-05-14 21:49:52 +0800

    5.14

---
+++ src/main/java/com/yuqiyu/MultiThread/SleepLock.java
@@ -0,0 +1,48 @@
+package com.yuqiyu.MultiThread;
+
+public class SleepLock {
+    private Object lock = new Object();
+
+
+    public static void main(String[] args) {
+        SleepLock sl = new SleepLock();
+        Thread ts =  sl.new ThreadSleep();
+        ts.start();
+        try {
+            System.out.println("Main Thread sleep");
+            Thread.sleep(2000);
+            System.out.println("Main Thread wake");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Thread tns = sl.new ThreadNoSleep();
+        tns.start();
+
+    }
+
+    private class ThreadSleep extends Thread{
+        @Override
+        public void run() {
+            synchronized (lock){
+                try {
+                    System.out.println("ThreadSleep get lock");
+                    Thread.sleep(5000);
+                    System.out.println("ThreadSleep release lock");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
+    private class ThreadNoSleep extends Thread{
+        @Override
+        public void run() {
+            synchronized (lock){
+                System.out.println("ThreadNoSleep get lock");
+            }
+        }
+    }
+}
