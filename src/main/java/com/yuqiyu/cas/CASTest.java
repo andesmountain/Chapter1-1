de704dfef5cb109eb71ce761a2eb95136d5ea209
andesmountain <443889956@qq.com>
2019-05-23 21:50:46 +0800

    1

--- src/main/java/com/yuqiyu/cas/CASTest.java
+++ src/main/java/com/yuqiyu/cas/CASTest.java
@@ -1,9 +1,13 @@
 package com.yuqiyu.cas;
 
+import lombok.Data;
+import org.apache.catalina.User;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicIntegerArray;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicStampedReference;
 
 /**
  * 版权: Copyright (c) 2011-2018 <br>
@@ -28,8 +32,51 @@ public class CASTest {
         arr.getAndSet(0,5);
         System.out.println(arr);
         System.out.println(value[0]);
+    }
+
+
+    @Data
+    static class UserInfo{
+        String name;
+        int age;
+        public UserInfo(String name,int age){
+            this.name= name;
+            this.age = age;
+        }
+    }
+
+
+    @Test
+    public void userAtomicReference(){
+        UserInfo ui = new UserInfo("zhu",29);
+        AtomicReference<UserInfo> ar = new AtomicReference<>(ui);
+        UserInfo updateUser = new UserInfo("yin",28);
+        ar.compareAndSet(ui,updateUser);
+
+        System.out.println("替换后"+ar.get());
+        System.out.println("替换前"+ui);
 
     }
 
+
+    @Test
+    public void testAtomicStampedReference() throws InterruptedException {
+        AtomicStampedReference<String> asr = new AtomicStampedReference("abc",1);
+        Thread t1 = new Thread(()->{
+            System.out.println("t1 开始运行");
+            System.out.println(asr.getReference()+":"+asr.getStamp());
+            System.out.println("执行结果"+asr.compareAndSet("abc","lol",1,2));
+        });
+        t1.start();
+        t1.join();
+        Thread t2 = new Thread(()->{
+            System.out.println("t2 开始执行");
+            System.out.println(asr.getReference()+":"+asr.getStamp());
+            System.out.println("执行结果"+asr.compareAndSet("lol","abc",0,1));
+        });
+        t2.start();
+
+
+    }
 
 }
